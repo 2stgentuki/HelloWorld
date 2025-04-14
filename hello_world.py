86e71c46dce70ed4fa9d7c6ce4222850a55a72b8
2stgentuki <2stgentuki@gmail.com>
2025-04-14 21:29:39 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 import requests
 import streamlit as st
 
-dify_api_key = st.secrets["app-HuGeHCkqdR01kQoiMAXyzBwX"]
+dify_api_key = st.secrets[""]
 url = 'https://api.dify.ai/v1/chat-messages'
 
 st.title('ロボ川のお悩み相談室')
